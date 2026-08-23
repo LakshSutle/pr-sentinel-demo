b9ebab323596ad5f24c02f9f1e3aa27570b4bcaf
Laksh Sutle <163562422+LakshSutle@users.noreply.github.com>
2026-08-23 12:39:02 +0530

    Add Flask app to retrieve user by email

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+def get_user(email):
+    conn = sqlite3.connect("users.db")
+    query = f"SELECT * FROM users WHERE email = '{email}'"
+    return conn.execute(query).fetchone()
+
+@app.route("/user")
+def user():
+    email = request.args.get("email")
+    return str(get_user(email))
